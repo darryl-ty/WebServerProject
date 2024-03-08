10c82db448d7b23998f5f2c32e8453b67c5a14f5
darryl-ty <darrylterrell921@gmail.com>
2024-03-08 02:16:31 -0600

    Implement error handling.

--- src/client.cpp
+++ src/client.cpp
@@ -9,24 +9,33 @@ int main(){
     wsaerr = WSAStartup(wVersion, &wsaData);
 
     if (wsaerr != 0) {
-        std::cout << "Windows Socket was unable to initialize." << std::endl;
+        std::cerr << "Windows Socket was unable to initialize." << std::endl;
         return 0;
     } else {
-        std::cout << "Windows Socket was successfully initialized with status: " << wsaData.szSystemStatus << std::endl;
+        std::clog << "Windows Socket was successfully initialized with status: " << wsaData.szSystemStatus << std::endl;
     }
 
     SOCKET clientSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     if (clientSocket == INVALID_SOCKET){
-        std::cout << "Unable to create socket. Following error: " << WSAGetLastError() << std::endl;
+        std::cerr << "Unable to create socket. Following error: " << WSAGetLastError() << std::endl;
         WSACleanup();
         return 1;
     } else {
-        std::cout << "Successfully created client-side socket!" << std::endl;
+        std::clog << "Successfully created client-side socket!" << std::endl;
     }
 
     SOCKADDR_IN serverAddr{AF_INET,
                            htons(2468),
                            static_cast<u_char>(inet_addr("127.0.0.1"))};
+
+    int result = connect(clientSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr));
+    if (result == SOCKET_ERROR){
+        std::cerr << "Unable to connect to server. Following error: " << WSAGetLastError() << std::endl;
+        WSACleanup();
+        return 1;
+    } else {
+        std::clog << "Connection to server established!" << std::endl;
+    }
 
 
 
