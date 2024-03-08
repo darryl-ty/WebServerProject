761b697234e61a74edd55b6ab74dc4e063ae9ba4
darryl-ty <darrylterrell921@gmail.com>
2024-03-08 16:36:11 -0600

    Creation and error handling of serverSocket.

--- src/server.cpp
+++ src/server.cpp
@@ -1,7 +1,18 @@
 #include <winsock2.h>
-#include "server.h"
+#include <fstream>
+#include <iostream>
+
+SOCKET createSocket(int portNum);
 
 int main(){
+    const int portNum = 2468;
+
+    SOCKET server = createSocket(portNum);
+
+    return 0;
+}
+
+SOCKET createSocket(int portNum) {
     WSAData wsaData;
     int wsaerr;
     WORD wVersion = MAKEWORD(2, 2);
@@ -9,9 +20,17 @@ int main(){
 
     if (wsaerr != 0) {
         std::cerr << "Windows Socket was unable to initialize." << std::endl;
-        return 0;
+        return 1;
     } else {
         std::clog << "Windows Socket was successfully initialized with status: " << wsaData.szSystemStatus << std::endl;
     }
-    return 0;
+
+    SOCKET serverSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (serverSocket == INVALID_SOCKET){
+        std::cerr << "Unable to create socket. Following error: " << WSAGetLastError() << std::endl;
+        WSACleanup();
+        return 1;
+    } else {
+        std::clog << "Successfully created client-side socket!" << std::endl;
+    }
 }
