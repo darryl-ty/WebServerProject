fea8769fbab985c3ef46ff3d3bf1dc19bc1e41c0
darryl-ty <darrylterrell921@gmail.com>
2024-03-08 02:19:58 -0600

    Implement error handling.

--- src/client.cpp
+++ src/client.cpp
@@ -3,14 +3,14 @@
 
 
 int main(){
-    WSAData wsaData{};
+    WSAData wsaData;
     int wsaerr;
     WORD wVersion = MAKEWORD(2, 2);
     wsaerr = WSAStartup(wVersion, &wsaData);
 
     if (wsaerr != 0) {
         std::cerr << "Windows Socket was unable to initialize." << std::endl;
-        return 0;
+        return 1;
     } else {
         std::clog << "Windows Socket was successfully initialized with status: " << wsaData.szSystemStatus << std::endl;
     }
